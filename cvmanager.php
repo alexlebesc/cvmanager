9d7def3cd8007d1b6a49157c7fb58cbeb810e704
Alex Le Bescond <alex.lebescond@gmail.com>
2015-09-14 11:30:17 +0200

    55d0912eb2543aa7e08ea750 create xml format to store CV content

--- cvmanager.php
+++ cvmanager.php
@@ -7,3 +7,25 @@
  */
 
 echo "welcome in CV manager";
+
+/**
+ * cvmanager build
+ *
+ * take the xml data
+ * take the xslt
+ * build index.html
+ */
+
+$xslFile = __DIR__ . "/mycv.xslt";
+$xmlFile = __DIR__ . "/data.xml";
+
+$xslDoc = new DOMDocument();
+$xslDoc->load($xslFile);
+
+$xmlDoc = new DOMDocument();
+$xmlDoc->load($xmlFile);
+
+$proc = new XSLTProcessor();
+$proc->importStylesheet($xslDoc);
+
+$proc->transformToUri($xmlDoc, 'file://'.getcwd().'/frontend/mycv/index.html');
